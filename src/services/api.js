45c08b185220c2c018e39dde42482e4fcecdfbac
Rohit3kushwah <rk6109744@gmail.com>
2025-09-01 02:01:54 -0500

    add

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://server-event-booking.onrender.com/api',
   withCredentials: true, // for cookies
 });
 
